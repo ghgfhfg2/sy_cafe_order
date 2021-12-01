8c565a524911781b45de55311c1860f9863cb8eb
hyunsoo kim <ghgfhfg2@naver.com>
2021-12-02 08:44:15 +0900

    .

--- craco.config.js
+++ craco.config.js
@@ -7,7 +7,7 @@ module.exports = {
       options: {
         lessLoaderOptions: {
           lessOptions: {
-            modifyVars: { '@primary-color': '#7c5424' },
+            modifyVars: { '@primary-color': '#b93530' },
             javascriptEnabled: true,
           },
         },
